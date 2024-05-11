4c34af1bdfec6e3274bb3ed0d2e478477bc28b1b
Zayleth <zaylethvegas.com>
2024-05-11 19:13:29 -0400

    Indentation Changes Pt2

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -11,50 +11,61 @@ def valid_word(words):
 
     return word.upper()
 
-# we defined the function and add the variables.
+# we defined the function and add the parameters.
 def hangman():
-    word = valid_word
-    word_letters = set(word) #letters in the word
+    word = valid_word(words)                   #Llama a valid_word para obtener la palabra elegida
+    word_letters = set(valid_word(words))      #letters in the word. word_letters llama a la función valid_word, que recupera una palabra aleatoria sin guiones ni espacios como se desea
+                                               #mediante la funcion set(...) que toma la cadena devuelta (la palabra) y crea un conjunto que contiene sus letras únicas.
     alphabet = set(string.ascii_uppercase)
-    used_letters = set() #letters that user has guessed
+    used_letters = set()                       #letters that user has guessed
+
     lives = 10
+
 
     #user input
     while len(word_letters) > 0 and lives > 0:
-        print('You have', lives, 'and you have used these letters: ', ' '.join(used_letters))
 
     #letters used
     # ' '. join (['a', 'b', 'c']) --> 'a b c'
+        print('You have', lives, 'lives and you have used these letters: ', ' '.join(used_letters))
 
-    #current word is 
-    word_list = [letter if letter in used_letters else '-' for letter in word]      #we create a new variable 'word_list'. 
+        #current word is (for example Z A Y L E - H)
+        word_list = [letter if letter in used_letters else '-' for letter in word]  
+        
+        """
+        we create a new variable 'word_list'. 
+        Si la letter se encuentra en el conjunto used_letters (lo que significa que se ha adivinado correctamente), la letra en sí se agrega a la lista word_list.
+        De lo contrario, se agrega un guión (-) a la lista.
+        """
 
-    print('Current word: ', ' '.join(word_list))
+        print('Current word: ', ' '.join(word_list))
 
-    user_letter = input("Guess a letter: ").upper()
 
-    if user_letter in alphabet - used_letters:    #if user letter is in the alphabet and the letter has not been used.
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:      # if user letter is in the alphabet and the letter has not been used.
+            used_letters.add(user_letter)               # so, let's add user_letter to used_letters
+            
+            if user_letter in word_letters:             # if user_letter is in word_letters
+                word_letters.remove(user_letter)        # then, let's remove the user_letter from word_letters
+                print('')
+            
+            
+            else:
+                lives = lives - 1  # takes away a life if wrong 
+                print('\nYour letter,', user_letter, 'is not in the word. Try again!')
+                
 
-        used_letters.add(user_letter)               #so, let's add user_letter to used_letters
-        if user_letter in word_letters:             #if user_letter is in word_letters
-            word_letters.remove(user_letter)        #then, let's remove the user_letter from word_letters
+        elif user_letter in used_letters:
+            print('\nYou have already used that character. Please try again. ')
 
         else:
-            lives = lives - 1                       #takes away a life if wrong 
-            print('Letter is not in the word')
+            print('\nInvalid character. Please try again. ')
 
-
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again. ')
-
-    else:
-        print('Invalid character. Please try again. ')
-
-    #user gets here when len(word_letters) == 0 OR when lives == 0
+    #user gets here when len (word_letters) == 0 OR when lives == 0
     if lives == 0:
         print('Oh! You died. The word was', word)
 
-    else:
-        print('Yayy!. You guessed the world', word, '!')
+    if word_letters == 0:
+        print('Yayy!. You guessed the word', word, '!')
 
 hangman()
