ba699ffec791306019d96f68009e655c86932e87
Zayleth <zaylethvegas.com>
2024-05-11 19:17:07 -0400

    Projects order

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -13,23 +13,23 @@ def valid_word(words):
 
 # we defined the function and add the parameters.
 def hangman():
-    word = valid_word(words)                   #Llama a valid_word para obtener la palabra elegida
-    word_letters = set(valid_word(words))      #letters in the word. word_letters llama a la función valid_word, que recupera una palabra aleatoria sin guiones ni espacios como se desea
-                                               #mediante la funcion set(...) que toma la cadena devuelta (la palabra) y crea un conjunto que contiene sus letras únicas.
+    word = valid_word(words) # Llama a valid_word para obtener la palabra elegida
+    word_letters = set(valid_word(words)) # letters in the word. word_letters llama a la función valid_word, que recupera una palabra aleatoria sin guiones ni espacios como se desea
+                                          # mediante la funcion set(...) que toma la cadena devuelta (la palabra) y crea un conjunto que contiene sus letras únicas.
     alphabet = set(string.ascii_uppercase)
-    used_letters = set()                       #letters that user has guessed
+    used_letters = set() # letters that user has guessed
 
     lives = 10
 
 
-    #user input
+    # user input
     while len(word_letters) > 0 and lives > 0:
 
-    #letters used
+    # letters used
     # ' '. join (['a', 'b', 'c']) --> 'a b c'
         print('You have', lives, 'lives and you have used these letters: ', ' '.join(used_letters))
 
-        #current word is (for example Z A Y L E - H)
+        # current word is (for example Z A Y L E - H)
         word_list = [letter if letter in used_letters else '-' for letter in word]  
         
         """
@@ -42,16 +42,16 @@ def hangman():
 
 
         user_letter = input("Guess a letter: ").upper()
-        if user_letter in alphabet - used_letters:      # if user letter is in the alphabet and the letter has not been used.
-            used_letters.add(user_letter)               # so, let's add user_letter to used_letters
+        if user_letter in alphabet - used_letters: # if user letter is in the alphabet and the letter has not been used.
+            used_letters.add(user_letter) # so, let's add user_letter to used_letters
             
-            if user_letter in word_letters:             # if user_letter is in word_letters
-                word_letters.remove(user_letter)        # then, let's remove the user_letter from word_letters
+            if user_letter in word_letters: # if user_letter is in word_letters
+                word_letters.remove(user_letter) # then, let's remove the user_letter from word_letters
                 print('')
             
             
             else:
-                lives = lives - 1  # takes away a life if wrong 
+                lives = lives - 1  # takes away a life if wrong
                 print('\nYour letter,', user_letter, 'is not in the word. Try again!')
                 
 
@@ -65,7 +65,7 @@ def hangman():
     if lives == 0:
         print('Oh! You died. The word was', word)
 
-    if word_letters == 0:
-        print('Yayy!. You guessed the word', word, '!')
+    else:
+        print('Yayy!. You guessed the word', word, '!') #nunca sale mensaje de felcitaciones
 
 hangman()
